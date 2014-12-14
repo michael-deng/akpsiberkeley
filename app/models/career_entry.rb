34d5784499afcb8a58babe5ed69fbec21dc6a834
Michael Deng <michaeldeng18@gmail.com>
2014-12-13 18:35:24 -0800

    Fix fullintern

--- app/models/career_entry.rb
+++ app/models/career_entry.rb
@@ -6,7 +6,7 @@ class CareerEntry < ActiveRecord::Base
 	validates :name, presence: true
 	validates :company, presence: true
 	validates :year, presence: true, length: {is: 4}
-	validates :fullintern, presence: true
+	validates :fulltime_or_intern, presence: true
 	validates :location, presence: true
 	validates :job_category, presence: true
 
