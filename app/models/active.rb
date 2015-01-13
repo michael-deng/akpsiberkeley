a22b0a973a5eecac53be40716a5e9f8e25f0ab26
Michael Deng <michaeldeng18@gmail.com>
2015-01-13 03:58:06 -0800

    Find font for careers page

--- app/models/active.rb
+++ app/models/active.rb
@@ -7,7 +7,7 @@ class Active < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :photo, :styles => { :medium => "300x300>", :small => "200x200>", :thumb => "100x100>" }
+  has_attached_file :photo, :styles => { :medium => "300x300>", :small => "250x250>", :thumb => "100x100>" }
   validates_attachment :photo, :content_type => { :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"] }, :size => { :in => 0..5.megabytes }
 
   # Each active can have many career entries
