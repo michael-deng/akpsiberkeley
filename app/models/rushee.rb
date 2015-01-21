c76475348aa9aa3c8f3eb535aca38d0aec39fe5d
Michael Deng <michaeldeng18@gmail.com>
2015-01-21 10:26:25 -0800

    Use amazon s3 for paperclip storage

--- app/models/rushee.rb
+++ app/models/rushee.rb
@@ -7,11 +7,21 @@ class Rushee < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :photo, :styles => { :medium => "300x300>", :small => "200x200>", :thumb => "100x100>" }
-  validates_attachment :photo, :content_type => { :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"] }, :size => { :in => 0..5.megabytes }
+  has_attached_file :photo, 
+    :styles => { :medium => "300x300>", :small => "200x200>", :thumb => "100x100>" }
+  
+  validates_attachment :photo, 
+    :content_type => { :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"] }, 
+    :size => { :in => 0..5.megabytes }
 
   # These fields have to be filled out when a rushee signs up
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-
+  validates :name, 
+    presence: true, 
+    length: { maximum: 50 }
+  
+  validates :email, 
+    presence: true, 
+    length: { maximum: 255 }, 
+    format: { with: VALID_EMAIL_REGEX }, 
+    uniqueness: { case_sensitive: false }
 end
