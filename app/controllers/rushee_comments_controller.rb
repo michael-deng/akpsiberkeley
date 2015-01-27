36cd34f51898081de7a45cc446cf52fdf5987568
Michael Deng <michaeldeng18@gmail.com>
2015-01-27 13:17:05 -0800

    Add destroy capabilities to rushee comments

--- app/controllers/rushee_comments_controller.rb
+++ app/controllers/rushee_comments_controller.rb
@@ -1,5 +1,8 @@
 class RusheeCommentsController < ApplicationController
 
+	before_action :authenticate_active!, only: [:create, :destroy]
+	before_action :correct_active, only: :destroy
+
 	def create
 		@rushee_profile = RusheeProfile.find(params[:rushee_profile_id])
 		@rushee_comment = @rushee_profile.rushee_comments.build(rushee_comment_params)
@@ -15,6 +18,9 @@ class RusheeCommentsController < ApplicationController
 	end
 
 	def destroy
+		@rushee_profile = @rushee_comment.rushee_profile
+		@rushee_comment.destroy
+		redirect_to @rushee_profile
 	end
 
 	private
@@ -22,4 +28,9 @@ class RusheeCommentsController < ApplicationController
     def rushee_comment_params
       params.require(:rushee_comment).permit(:content)
     end
+
+    def correct_active
+			@rushee_comment = current_active.rushee_comments.find_by_id(params[:id])
+			redirect_to root_path if @rushee_comment.nil?
+		end
 end
