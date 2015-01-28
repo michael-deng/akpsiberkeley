3eb1c2cfc13893fa8d1bfa5bdbfe1b5c046f5f22
Michael Deng <michaeldeng18@gmail.com>
2015-01-28 15:24:08 -0800

    Make dropdown bars in header and create new page for eboard

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -23,4 +23,14 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+
+  def eboard_actives
+    @actives = Active.all.sort_by{ |a| a.name.split(' ')[a.name.split(' ').length - 1].downcase }
+    @eboard_actives = []
+    for i in 0 ... @actives.size
+      if !@actives[i].eboard_position.empty?
+        @eboard_actives.push(@actives[i])
+      end
+    end
+  end
 end
