5014b93d37a6b11b6fba673a70f8113c7552cd08
Michael Deng <michaeldeng18@gmail.com>
2015-01-27 22:37:45 -0800

    Fix error in rush application active admin

--- app/models/rush_application.rb
+++ app/models/rush_application.rb
@@ -1,2 +1,28 @@
 class RushApplication < ActiveRecord::Base
+
+	has_attached_file :resume
+	has_attached_file :cover_letter
+	has_attached_file :transcript
+
+	validates_attachment :cover_letter, :resume,
+                      content_type: {content_type: "application/pdf"},
+                      presence: true,
+                      size: {in: 0..1.megabytes}
+
+  validates_attachment :transcript,
+  										content_type: {content_type: "application/pdf"},
+  										size: {in: 0..1.megabytes}
+
+  validates :first_name, :last_name 
+    presence: true, 
+    length: { maximum: 50 }
+  
+  validates :email,
+    presence: true,
+    length: { maximum: 255 }, 
+    format: { with: VALID_EMAIL_REGEX }, 
+    uniqueness: { case_sensitive: false }
+
+
+
 end
