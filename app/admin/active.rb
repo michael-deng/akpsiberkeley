f1aa0d72fc06d40d2fc32af168d36c19b9db9523
Michael Deng <michaeldeng18@gmail.com>
2015-09-01 10:24:48 -0700

    Allow admin to change password

--- app/admin/active.rb
+++ app/admin/active.rb
@@ -44,10 +44,11 @@ ActiveAdmin.register Active do
       f.input :display, :label => "Show on brothers page"
       f.input :biography
       f.input :photo
-      if f.object.new_record?
-          f.input :password
-          f.input :password_confirmation
-      end
+      f.input :password
+      #if f.object.new_record?
+      #    f.input :password
+      #    f.input :password_confirmation
+      #end
     end
     f.actions
   end
